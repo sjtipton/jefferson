4cb5a7c7a412a080d692b1bc24a57d8f1693ea6d
Steve Tipton <steve.tipton@employmentguide.com>
2012-09-20 10:19:19 -0400

    Add initial requirements

--- lib/jefferson.rb
+++ lib/jefferson.rb
@@ -1,5 +1,16 @@
 require "jefferson/version"
 
 module Jefferson
-  # Your code goes here...
+  require 'yajl/json_gem'
+  require 'active_support/core_ext/hash'
+  require 'active_support/core_ext/object'
+  require 'active_model'
+  require 'typhoeus'
+  require 'sanitize'
+  require 'cgi'
+
+  $LOAD_PATH.unshift(File.dirname(__FILE__))
+
+  require 'jefferson/version.rb'
+  require 'jefferson/config.rb'
 end
